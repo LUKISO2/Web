4acf09db6834515063e6153fd7511ba178269844
LUKISO2 <lukisonej@gmail.com>
2021-03-28 10:58:30 +0200

    master

--- scripts/lighttheme.js
+++ scripts/lighttheme.js
@@ -7,7 +7,23 @@ window.onload=function() {
     document.body.classList.toggle("light-theme");
     if(this.textContent == darkStr) this.textContent = lightStr;
     else this.textContent = darkStr;
-    if(this.textContent == darkStr) document.cookie = "lighttheme=false"
-    else document.cookie = "lighttheme=true"
+    if(this.textContent == darkStr) document.cookie = "lighttheme=false";
+    else document.cookie = "lighttheme=true";
   });
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
