35ec303143eb060ab5b775131240970acc4f3a90
LUKISO2 <lukisonej@gmail.com>
2024-12-15 15:36:01 +0100

    Updated mobile interface

--- scripts/voucher.js
+++ scripts/voucher.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   const submitBtn = document.getElementById('submit-btn');
   const voucherInput = document.getElementById('voucher-input');
+  const errorMessage = document.getElementById('error-message');
+
 
   function handleSubmit() {
     const voucherCode = voucherInput.value.trim();
     if (voucherCode === "5318008") {
       window.location.href = "5318008.html";
     } else {
-      alert("Nesprávný kód. Zkuste to prosím znovu.");
+      errorMessage.textContent = "Nesprávný kód. Zkuste to prosím znovu.";
+      errorMessage.style.display = "block";
     }
   }
 
@@ -16,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Press Enter while focusing on the input
   voucherInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' || e.keyCode === 13) {
+    if (e.key === 'Enter') {
       handleSubmit();
     }
   });
